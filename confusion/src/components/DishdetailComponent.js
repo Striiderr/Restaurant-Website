72767d4606fec477514757f2f6728089ada3338c
Gaurav Rana <gauravrana@MacBook.local>
2022-07-27 12:51:30 +0530

    Functional Components

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -1,33 +1,76 @@
-import { Component } from "react";
+import React from "react";
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
 
-class DishDetail extends Component{
-    constructor(props){
-        super(props);
 
-        this.state={
-
-        };
+        
+    function RenderComments({comments}){
+        if (comments == null) {
+            return (<div></div>)
+        }
+        const cmnts = comments.map(comnt => {
+            return(
+                        <li key = {comnt.id} >
+                            <h5>{comnt.comment}</h5>
+                            <p>--{comnt.author}, &nbsp;
+                            {
+                            new Intl.DateTimeFormat('en-US',{
+                                year:'numeric',
+                                month:'short',
+                                day:'2-digit'}).format(new Date(comnt.date))
+                            }
+                            </p>
+                            <br></br>
+                        </li>
+            );
+        });
+        
+      return(
+        <div className="col-12 col-md-5 m-1">
+            <h2> Comments</h2>
+            <br></br>
+            <ul className="list-unstyled">
+                {cmnts}
+            </ul>
+        </div>
+      );  
     }
 
-    render(){
-    return(
-        <div className="col-12 col-md-5 m-1">
-           <Card key={this.props.selectedDish.id}></Card>    
-        </div>
-        // <div className="col-12 col-md-5 m-1">
-        //    <Card key={this.props.selectedDish.id}>
-        //     <CardImg width="100%" object src={this.props.selectedDish.image} alt={this.props.selectedDish.name}></CardImg>
-        //     <CardBody>
-        //         <CardTitle><h2>{this.props.selectedDish.name}</h2></CardTitle>
-        //         <CardText>{this.props.selectedDish.description}</CardText>
-        //     </CardBody>
-        //     </Card> 
-        //     </div>
+   function RenderCard({dish}){
+        if(dish!=null){
+            return(
+                <div className="col-12 col-md-5 m-1">
+                <Card key={dish.id}>
+                 <CardImg width="100%" object src={dish.image} alt={dish.name}></CardImg>
+                 <CardBody>
+                     <CardTitle ><h2>{dish.name}</h2></CardTitle>
+                     <CardText>{dish.description}</CardText>
+                 </CardBody>
+                 </Card> 
+                 </div>
+ 
+            );
+        }
+        else{
+        return(
+            <div></div>
+        );
+    }
+    }
 
-    );
-}
-}
+    const DishDetail=(props)=>{
+        const dish=props.dish;
+        if(dish){
+        return(  
+            <div className="container">
+            <div className="row">
+           <RenderCard dish={props.dish}/>
+           <RenderComments comments={props.dish.comments}/>
+           </div>
+           </div>
+        
+    );}
+    }
+       
 
 export default DishDetail;
